7daa98a3ca00f44cd9ebdaaf322f28b4c420e97b
Minh Nguyen <minhnguyen93@outlook.com>
2024-02-29 16:26:45 +0200

    Add phonenumber validation

--- models/person.js
+++ models/person.js
@@ -25,7 +25,13 @@ const personSchema = new mongoose.Schema({
   number: {
     type: String,
     minLength: 8,
-    required: true
+    required: [true, 'User phone number required'],
+    validate: {
+      validator: function(v) {
+        return /^\d{2}-\d{5,}$/.test(v) || /^\d{3}-\d{4,}$/.test(v)
+      },
+      message: props => `${props.value} is not a valid phone number!`
+    },
   }
 })
 personSchema.plugin(uniqueValidator)
